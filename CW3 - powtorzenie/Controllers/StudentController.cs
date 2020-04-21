3bb3112c3fc7887159a8386d8abe52b553674c7d
s18986 <s18986@users.noreply.github.com>
2020-04-21 13:31:31 +0200

    Poprawienie zestawu 3
    Zrobienie zestawu od nowa poniewarz wczesniej  nie odpowiadal na niego postman oraz naprawienie koncowek w ramahc powtorzenia

---
+++ CW3 - powtorzenie/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW3___powtorzenie.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult getStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            else
+            {
+                return NotFound("Nie znaleziono takiego studenta");
+            }
+        }
+
+        [HttpGet]
+        public string getStudents(string Orderby)
+        {
+            return $"Kowalski, Malewski, Andrzejewski sortowanie={Orderby}";
+        }
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.indexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id)
+        {
+            return Ok("Aktualizacja dokonczona");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Usuwanie dokonczone");
+        }
+    }
+}
---
+++ CW3 - powtorzenie/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW3___powtorzenie
+{
+    public class Student
+    {
+        public int idStudent { get; set; }
+        public String Firstname { get; set; }
+        public String Lastname { get; set; }
+        public String indexNumber { get; set; }
+    }
+}
